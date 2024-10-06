b2a39d90d664454f1a0dd2ad98bc9ed23dedb2ee
code jstar <codejstar@gmail.com>
2024-10-06 18:25:53 +0530

    create form in index.html and mapping with the getMethod

--- src/main/java/org/codingwala/dummy/DummyApplication.java
+++ src/main/java/org/codingwala/dummy/DummyApplication.java
@@ -9,7 +9,7 @@ public class DummyApplication {
 	public static void main(String[] args) {
 		MyController.myMethod();
 		SpringApplication.run(DummyApplication.class, args);
-		System.out.println("Hello World!");
+		// System.out.println("Hello World!");
 	}
 
 }
--- src/main/java/org/codingwala/dummy/MyController.java
+++ src/main/java/org/codingwala/dummy/MyController.java
@@ -2,6 +2,8 @@ package org.codingwala.dummy;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class MyController {
@@ -10,4 +12,10 @@ public class MyController {
     public static String myMethod() {
         return "Jagdeep Jakhu Methods";
     }
+
+    @GetMapping("process-form")
+    public String getData() {
+        return "<h1>Process Form</h1>";
+    }
+
 }
